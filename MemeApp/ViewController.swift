b186023b0dfe8e8fbc8324a8421ae9e758a29d2d
avendisianipar <avendi.sianipar@gli.id>
2021-04-16 15:46:06 +0700

    Initial Commit

---
+++ MemeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeApp
+//
+//  Created by Avendi Sianipar on 16/04/21.
+//  Copyright © 2021 Avendi Sianipar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
